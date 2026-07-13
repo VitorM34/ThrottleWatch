96d678a173860de70c575a592c514a6fe4327667
VitorM34 <vitorluistresoldiluis@outlook.com>
2026-07-13 10:09:01 -0300

    chore(backend): set up solution structure and project references

---
+++ tests/ThrottleWatch.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ThrottleWatch.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/ThrottleWatch.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ThrottleWatch.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
